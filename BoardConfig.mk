b1d83e888bddc95f44ecf4d6b3aa65da74e9c2c9
Martinusbe <martinusbe@gmail.com>
2016-08-17 13:17:10 +0200

    build with clang
    Change-Id: I58c2455feba402d1716de33a7747e8543751f982
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,6 +56,9 @@ ENABLE_CPUSETS := true
 
 TARGET_USES_64_BIT_BINDER := true
 
+# Clang
+USE_CLANG_PLATFORM_BUILD := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
