f8365d1b16510e632332c3ec4458334e3b3ff5b1
Martinusbe <martinusbe@gmail.com>
2017-10-25 17:17:17 +0200

    op3: create validus dependencies

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,7 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
 #TARGET_USES_HWC2 := true
 
 # Enable dexpreopt to speed boot time
-WITH_DEXPREOPT := true
+WITH_DEXPREOPT := false
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
