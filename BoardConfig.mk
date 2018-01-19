c5387e63acc2ee0af8436c69db1fb6993f373544
martinusbe <martinusbe@gmail.com>
2018-01-19 23:36:22 +0100

    op3: add 15s wifi driver probe delay
    Change-Id: I7c67cb2a88b4c1a9751fb1cfb74e42d0e39fce9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,6 +235,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_PROBE_DELAY := 15
 
 # inherit from the proprietary version
 -include vendor/oneplus/oneplus3/BoardConfigVendor.mk
