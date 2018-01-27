e4113a2750b3e518ad0f2a9be3a5f9040868efa6
martinusbe <martinusbe@gmail.com>
2018-01-27 10:45:23 +0100

    Revert "op3: add some IMS"
    This reverts commit 6546008341904cc6442e993857dd69d01b1663d6.
    
    Change-Id: I91a9e35efa1b619833e9e4ba948afc59188d6392

--- device.mk
+++ device.mk
@@ -410,14 +410,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
-# Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext \
-    ims-ext-common
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Thermal
 PRODUCT_PACKAGES += \
     thermal.msm8996 \
